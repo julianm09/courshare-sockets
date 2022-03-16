772dd877ffbd61ebc1e6144eef010b53a3941251
Julian Mayes <juliantmayes@gmail.com>
2022-03-15 22:01:33 -0700

    first commit

--- index.js
+++ index.js
@@ -17,22 +17,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-const allusers = {};
-
 io.on("connection", (socket) => {
-  allusers[socket.id] = { left: 0, top: 0 };
-  io.emit("user_connected", allusers);
 
-  console.log("a user connected");
-  /* io.emit("change", socket.id); */
-  socket.on("alert_all", (txt) => {
-    io.emit("change", socket.id, txt);
-  });
-
-  socket.on("mouse_moved", (x, y) => {
-    socket.broadcast.emit("change_mouse", x, y, socket.id);
-    /* io.emit("change_mouse", x, y); */
-  });
 });
 
 server.listen(PORT, () => {
